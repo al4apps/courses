43b90aaf7b7d8e3b28a0c4263498c4d16f18191e
Alisher Abdurashidov <abdurashidov.au@gmail.com>
2025-03-26 17:25:57 +0700

    [+] Добавление экрана Входа.
    Добавил экран LoginFragment. Добавил стили для InputLayout, стили текста.

--- app/src/main/java/com/al4apps/courses/MainActivity.kt
+++ app/src/main/java/com/al4apps/courses/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.al4apps.courses
 
 import android.os.Bundle
+import android.view.ViewGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updateLayoutParams
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
