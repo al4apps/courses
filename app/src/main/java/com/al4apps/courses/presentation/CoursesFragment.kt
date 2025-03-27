8b518b3dca2fe182d019d781417a059be3cbbd16
Alisher Abdurashidov <abdurashidov.au@gmail.com>
2025-03-27 16:04:24 +0700

    [+] Добавление адаптера для списка курсов
    Добавил и настроил CoursesAdapter

--- app/src/main/java/com/al4apps/courses/presentation/CoursesFragment.kt
+++ app/src/main/java/com/al4apps/courses/presentation/CoursesFragment.kt
@@ -1,9 +1,84 @@
 package com.al4apps.courses.presentation
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.al4apps.courses.databinding.FragmentCoursesBinding
+import com.al4apps.courses.domain.Course
+import com.al4apps.courses.presentation.adapter.CoursesAdapter
 import com.al4apps.courses.utils.AbstractFragment
+import com.al4apps.courses.utils.autoCleared
 
 class CoursesFragment: AbstractFragment<FragmentCoursesBinding>(FragmentCoursesBinding::inflate) {
 
+    private var coursesAdapter: CoursesAdapter by autoCleared()
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initAdapter()
+        coursesAdapter.submitList(generateCourses())
+    }
+
+    private fun initAdapter() {
+        coursesAdapter = CoursesAdapter {
+            Toast.makeText(requireContext(), "id курса: $it", Toast.LENGTH_SHORT).show()
+        }
+        binding.coursesRecyclerView.adapter = coursesAdapter
+    }
+
+    private fun generateCourses(): List<Course> {
+        return listOf(
+            Course(
+                id = 100,
+                title = "Java-разработчик с нуля",
+                text = "Освойте backend-разработку и программирование на Java, фреймворки Spring и Maven, работу с базами данных и API. Создайте свой собственный проект, собрав портфолио и став востребованным специалистом для любой IT компании.",
+                price = "999",
+                rate = "4.9",
+                startDate = "2024-05-22",
+                hasLike = false,
+                publishDate = "2024-02-02"
+            ),
+            Course(
+                id = 101,
+                title = "3D-дженералист",
+                text = "Освой профессию 3D-дженералиста и стань универсальным специалистом, который умеет создавать 3D-модели, текстуры и анимации, а также",
+                price = "12 000",
+                rate = "3.9",
+                startDate = "2024-09-10",
+                hasLike = false,
+                publishDate = "2024-01-20"
+            ),
+            Course(
+                id = 102,
+                title = "Python Advanced. Для продвинутых",
+                text = "Вы узнаете, как разрабатывать гибкие и высокопроизводительные серверные приложения на",
+                price = "1 299",
+                rate = "4.3",
+                startDate = "2024-10-12",
+                hasLike = true,
+                publishDate = "2024-08-10"
+            ),
+            Course(
+                id = 103,
+                title = "Системный аналитик",
+                text = "Освоите навыки системной аналитики с нуля за 9 месяцев. Будет очень много практики на реальных проектах, чтобы вы могли сразу старто",
+                price = "1 199",
+                rate = "4.5",
+                startDate = "2024-04-15",
+                hasLike = false,
+                publishDate = "2024-01-13"
+            ),
+            Course(
+                id = 104,
+                title = "Аналитик данных",
+                text = "В этом уроке вы узнаете, кто такой аналитик данных и какие задачи он решает. А главное — мы расска",
+                price = "899",
+                rate = "4.7",
+                startDate = "2024-06-20",
+                hasLike = false,
+                publishDate = "2024-03-12"
+            )
+
+        )
+    }
 }
---
+++ app/src/main/java/com/al4apps/courses/presentation/adapter/CoursesAdapter.kt
@@ -0,0 +1,66 @@
+package com.al4apps.courses.presentation.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.al4apps.courses.R
+import com.al4apps.courses.databinding.CardCourseBinding
+import com.al4apps.courses.domain.Course
+import com.bumptech.glide.Glide
+
+class CoursesAdapter(
+    private val onClick: (id: Int) -> Unit
+) : ListAdapter<Course, CourseViewHolder>(CoursesDiffUtil()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CourseViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = CardCourseBinding.inflate(layoutInflater, parent, false)
+        return CourseViewHolder(binding, onClick)
+    }
+
+    override fun onBindViewHolder(holder: CourseViewHolder, position: Int) {
+        holder.bind(currentList[position])
+    }
+
+    class CoursesDiffUtil : DiffUtil.ItemCallback<Course>() {
+
+        override fun areItemsTheSame(oldItem: Course, newItem: Course): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Course, newItem: Course): Boolean {
+            return oldItem == newItem
+        }
+    }
+}
+
+class CourseViewHolder(
+    private val binding: CardCourseBinding,
+    private val onClick: (id: Int) -> Unit
+) : RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(course: Course) {
+        with(binding) {
+            binding.root.setOnClickListener {
+                onClick(course.id)
+            }
+
+            courseTitleTextView.text = course.title
+            courseDescriptionTextView.text = course.text
+            coursePriceTextView.text =
+                itemView.resources.getString(R.string.course_price_text, course.price)
+            ratingTextView.text = course.rate
+            dateTextView.text = course.startDate
+            if (course.hasLike) {
+                markImageView.setImageResource(R.drawable.mark_filled)
+            } else {
+                markImageView.setImageResource(R.drawable.mark)
+            }
+            Glide.with(itemView)
+                .load(R.drawable.course_image)
+                .into(mainImageView)
+        }
+    }
+}
